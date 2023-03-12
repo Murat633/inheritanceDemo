137e024bf352fcf1a769e81053f03bf72ec545fd
Murat Akyol <95498019+Murat633@users.noreply.github.com>
2023-03-12 11:14:27 +0300

    inheritanceDemo

---
+++ src/BaseKrediManager.java
@@ -0,0 +1,13 @@
+public class BaseKrediManager {
+
+    public BaseKrediManager(String text) {
+        System.out.println(text);
+    }
+
+    public BaseKrediManager(){
+        System.out.println("BaseKrediManager Runned!");
+    }
+    public void Hesapla(){
+        System.out.println("indirimler hesaplandı");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        OgretmenKrediManager ogretmenKrediManager = new OgretmenKrediManager();
+        ogretmenKrediManager.Hesapla();
+    }
+}
